6adf71b0ab792cf894cad035e1b4ea32d9ac6c6e
Mohit Joshi <40639379+joshiCoding@users.noreply.github.com>
2026-07-19 11:21:17 +0530

    Add module docstring and version to __init__.py
    Added module docstring and version information.

--- blind_transpiler/__init__.py
+++ blind_transpiler/__init__.py
@@ -1,2 +1,16 @@
-from .controllers.orchestrator import BQC, bqc
+"""
+BlindTranspiler
+
+A Python library for Universal Blind Quantum Computation and Quantum Homomorphic Encryption.
+"""
+
+__version__ = "1.0.0"
+
+from .controllers.ubqc import BQC
+from .controllers.qhe import bqc
+
+__all__ = [
+    "BQC",
+    "bqc",
+]
 
